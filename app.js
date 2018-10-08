20cd88117cb282a6612d8d11d9ac9538822ea057
unknown <01secn@gmail.com>
2018-10-08 14:04:35 +0100

    Moved dashboard into folder structure Server now loading dashboard

--- app.js
+++ app.js
@@ -6,10 +6,6 @@ const app = express();
 
 app.use(cors());
 
-app.use('/', (request, response) => {
-  response.sendFile(`${__dirname}/public/sandbox.html`);
-});
-
-app.use(express.static('public'));
+app.use(express.static('frontend'));
 
 app.listen(3004);
